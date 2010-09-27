d63cf6551db84f51faa9f8b4591055f93381564d
Alexander Kislitsky <sand8080@gmail.com>
2010-09-27 13:08:26 +0400

    Loading tests temporary excluded from all tests runner

--- src/helixauth/helixauth_tests.py
+++ src/helixauth/helixauth_tests.py
@@ -1,12 +1,13 @@
 import unittest
 
+from helixauth.test.logic.test_login import LoginTestCase #IGNORE:W0611 @UnusedImport
 from helixauth.test.logic.test_environment import EnvironmentTestCase #IGNORE:W0611 @UnusedImport
 from helixauth.test.logic.test_action_log import ActionLogTestCase #IGNORE:W0611 @UnusedImport
 
 from helixauth.test.security.test_security import SecurityTestCase #IGNORE:W0611 @UnusedImport
 
 from helixauth.test.wsgi.test_protocol import ProtocolTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.wsgi.test_application_loading import ApplicationTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.wsgi.test_application_loading import ApplicationTestCase #IGNORE:W0611 @UnusedImport
 
 
 if __name__ == '__main__':
