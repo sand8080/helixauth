d41e77f65673ce5410d2326277f47a5faeb1bcdd
sand8080 <sand8080@gmail.com>
2011-04-09 19:33:15 +0400

    Nosetests used in helixauth_tests

--- src/helixauth/helixauth_tests.py
+++ src/helixauth/helixauth_tests.py
@@ -1,17 +1,24 @@
-import unittest
+#import unittest
+#
+#from helixauth.test.wsgi.test_protocol import ProtocolTestCase #IGNORE:W0611 @UnusedImport
+#
+#from helixauth.test.logic.test_login import LoginTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_logout import LogoutTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_environment import EnvironmentTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_user import UserTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_service import ServiceTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_group import GroupTestCase #IGNORE:W0611 @UnusedImport
+#from helixauth.test.logic.test_access import AccessTestCase #IGNORE:W0611 @UnusedImport
+#
+#from helixauth.test.logic.test_action_log import ActionLogTestCase #IGNORE:W0611 @UnusedImport
 
-from helixauth.test.wsgi.test_protocol import ProtocolTestCase #IGNORE:W0611 @UnusedImport
 
-from helixauth.test.logic.test_login import LoginTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_logout import LogoutTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_environment import EnvironmentTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_user import UserTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_service import ServiceTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_group import GroupTestCase #IGNORE:W0611 @UnusedImport
-from helixauth.test.logic.test_access import AccessTestCase #IGNORE:W0611 @UnusedImport
+#if __name__ == '__main__':
+#    unittest.main()
 
-from helixauth.test.logic.test_action_log import ActionLogTestCase #IGNORE:W0611 @UnusedImport
+
+import os
 
 
 if __name__ == '__main__':
-    unittest.main()
+    os.system('nosetests')
