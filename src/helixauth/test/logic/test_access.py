114e8d7557fc4df8ccae3aaac0e1cfbadd0e1bc7
sand8080 <sand8080@gmail.com>
2011-04-12 23:03:20 +0400

    Access prohibiton for users in inactive group test covered

--- src/helixauth/test/logic/test_access.py
+++ src/helixauth/test/logic/test_access.py
@@ -63,6 +63,53 @@ class AccessTestCase(ActorLogicTestCase):
             'property': 'add_user'}
         self.assertRaises(RequestProcessingError, self.check_access, **req)
 
+    def test_access_denied_when_group_inactive(self):
+        self.create_actor_env()
+        sess_id = self.login_actor()
+
+        # adding limited user
+        req = {'session_id': sess_id, 'filter_params': {'name': 'Users'},
+            'paging_params': {}}
+        resp = self.get_groups(**req)
+        self.check_response_ok(resp)
+        groups = resp['groups']
+        self.assertEqual(1, len(groups))
+        grp = groups[0]
+        self.assertEqual(True, grp['is_active'])
+
+        req = {'session_id': sess_id, 'login': 'u0', 'password': 'p0',
+            'groups_ids': [grp['id']]}
+        resp = self.add_user(**req)
+        self.check_response_ok(resp)
+        env = self.get_environment_by_name(self.actor_env_name)
+
+        # login limited user
+        req = {'environment_name': env.name, 'login': 'u0', 'password': 'p0'}
+        resp = self.login(**req)
+        self.check_response_ok(resp)
+        u_sess_id = resp['session_id']
+
+        # checking access granted to service properties
+        req = {'session_id': u_sess_id, 'service_type': Service.TYPE_AUTH,
+            'property': 'check_access'}
+        resp = self.check_access(**req)
+        self.check_response_ok(resp)
+
+        # inactivating group
+        req = {'session_id': sess_id, 'id': grp['id'], 'new_is_active': False}
+        resp = self.modify_group(**req)
+        self.check_response_ok(resp)
+
+        # checking access denied to service properties (in new session)
+        req = {'environment_name': env.name, 'login': 'u0', 'password': 'p0'}
+        resp = self.login(**req)
+        self.check_response_ok(resp)
+        u_sess_id = resp['session_id']
+
+        req = {'session_id': u_sess_id, 'service_type': Service.TYPE_AUTH,
+            'property': 'check_access'}
+        self.assertRaises(RequestProcessingError, self.check_access, **req)
+
 
 if __name__ == '__main__':
     unittest.main()
