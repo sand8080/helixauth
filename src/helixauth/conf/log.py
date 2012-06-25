ab2bf872c5256fab043925f85fe4a5b184f1a9dd
Alexander Kislitsky <sand8080@gmail.com>
2012-06-25 14:07:16 +0400

    Config fixes

--- src/helixauth/conf/log.py
+++ src/helixauth/conf/log.py
@@ -2,6 +2,6 @@ from helixcore.misc.log import init_logger
 from settings import log_filename, log_level, log_console, log_format
 
 
-logger = init_logger('helixbilling', log_filename, log_level,
+logger = init_logger('helixauth', log_filename, log_level,
     log_console=log_console, log_format=log_format
 )
