65268ccafe04b74df3e299a5b2e54a276067ed66
Alexander Kislitsky <akislitsky@loyalty-partners.ru>
2012-07-24 17:02:28 +0400

    Fix for ssl decryption error in mule process
    Postgresql SSL connection failed if transaction
    started in wsgi applicaton and in mule processes

--- uwsgi/scripts/mule_sessions_cleaner.py
+++ uwsgi/scripts/mule_sessions_cleaner.py
@@ -1,12 +1,26 @@
 import datetime
 from time import sleep
+from functools import partial
+import psycopg2
+from psycopg2 import extensions
+from psycopg2 import pool
+extensions.register_type(psycopg2.extensions.UNICODE)
 
 from helixcore import mapping
+import helixcore.db.wrapper as wrapper
 
-from helixauth.conf import settings
-from helixauth.conf.db import transaction
 from helixauth.conf.log import sess_logger as logger
 from helixauth.db.filters import SessionFilter
+from helixauth.conf import settings
+
+
+cp = pool.SimpleConnectionPool(1, 2, user=settings.DSN['user'],
+    database=settings.DSN['database'], host=settings.DSN['host'],
+    password=settings.DSN['password'])
+get_connection = cp.getconn
+put_connection = cp.putconn
+
+transaction = partial(wrapper.transaction, get_connection, put_connection)
 
 
 @transaction()
