cc87ddd58ffcb03f3abe420f88e075c69058a23a
Alexander Kislitsky <sand8080@gmail.com>
2012-12-16 12:37:15 +0400

    Sessiongs cleaning logic moved to session_utils

---
+++ src/helixauth/logic/session_utils.py
@@ -0,0 +1,28 @@
+import datetime
+from time import sleep
+
+from helixcore import mapping
+
+from helixauth.conf.db import transaction
+from helixauth.conf import settings
+from helixauth.conf.log import logger
+from helixauth.db.filters import SessionFilter
+
+
+def clean(curs):
+    to_d = datetime.datetime.now()
+    logger.info('Sessions cleaning initiated %s', to_d)
+    to_d = to_d - datetime.timedelta(minutes=settings.session_valid_minutes)
+    logger.debug('Removing session older %s. Session valid period: %s minutes',
+        to_d, settings.session_valid_minutes)
+    f = SessionFilter({'to_update_date': to_d}, {}, None)
+    sessions = f.filter_objs(curs)
+    logger.info('Deleting %s sessions', len(sessions))
+    mapping.delete_objects(curs, sessions)
+    logger.info('Sessions cleaned')
+
+
+def run():
+    while True:
+        clean()
+        sleep(seconds=settings.session_cleaning_minutes * 60)
--- uwsgi/scripts/mule_sessions_cleaner.py
+++ uwsgi/scripts/mule_sessions_cleaner.py
@@ -12,6 +12,7 @@ import helixcore.db.wrapper as wrapper
 from helixauth.conf.log import sess_logger as logger
 from helixauth.db.filters import SessionFilter
 from helixauth.conf import settings
+from helixauth.logic.session_utils import clean
 
 
 cp = pool.SimpleConnectionPool(1, 2, user=settings.DSN['user'],
@@ -24,22 +25,16 @@ transaction = partial(wrapper.transaction, get_connection, put_connection)
 
 
 @transaction()
-def clean(curs=None):
-    to_d = datetime.datetime.now()
-    logger.info('Sessions cleaning initiated %s', to_d)
-    to_d = to_d - datetime.timedelta(minutes=settings.session_valid_minutes)
-    logger.debug('Removing session older %s. Session valid period: %s minutes',
-        to_d, settings.session_valid_minutes)
-    f = SessionFilter({'to_update_date': to_d}, {}, None)
-    sessions = f.filter_objs(curs)
-    logger.info('Deleting %s sessions', len(sessions))
-    mapping.delete_objects(curs, sessions)
-    logger.info('Sessions cleaned')
+def _clean(curs=None):
+    """
+    function for transaction wrapping
+    """
+    clean(curs)
 
 
 def run():
     while True:
-        clean()
+        _clean()
         logger.debug('Sleeping %s minutes', settings.session_cleaning_minutes)
         sleep(settings.session_cleaning_minutes * 60)
 
